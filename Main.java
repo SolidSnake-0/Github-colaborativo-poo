5c4177559aac9d07f9effe0e9a402f7e6bf928b9
SolidSnake-0 <d.araneda05@ufromail.cl>
2024-09-13 17:20:55 -0300

    Primera Actividad de Hola Mundo Parte 2

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.printf("Hola mundo");
+        System.out.printf("Mensaje enviado por: Diego Araneda");
     }
 }
+
+
+
+
